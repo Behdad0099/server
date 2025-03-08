1afa2fba5e0b3bce8ca6f9644d9030875124c72b
Behdad Jafari <40280462-behdadjafari03@users.noreply.replit.com>
2025-03-08 15:56:27 +0000

    Restored to '73abef2b58cb7c9b6fa8945069fb4137ffba50c5'
    Replit-Restored-To: 73abef2b58cb7c9b6fa8945069fb4137ffba50c5

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ setTimeout(() => {
 
     // Don't call app.listen as mineboty is already using port 3000
     console.log('Express routes added to existing web server on port 3000');
-    console.log('Access bot status at: https://GameZone-mvtD.repl.co/status');
-    console.log('Main bot web interface: https://GameZone-mvtD.repl.co');
+    console.log('Access bot status at: http://yourrepl.repl.co/status');
 }, 5000);
 
